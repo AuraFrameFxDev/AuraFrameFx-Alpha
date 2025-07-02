cea2055c18db181b41a91d3c4c477cefd6d8645c
AuraKai <wehttam1989@gmail.com>
2025-07-01 21:38:49 -0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,13 +37,13 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_24
+        targetCompatibility = JavaVersion.VERSION_24
     }
 
     kotlin {
-        jvmToolchain(17)
-    }
+        jvmToolchain(24)
+    
 
     java {
         toolchain {
