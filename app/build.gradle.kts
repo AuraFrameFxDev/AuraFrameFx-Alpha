e4fe9ccfdf90e0b4258979ba9cad04fb6011ec96
AuraKai <wehttam1989@gmail.com>
2025-07-04 16:22:54 -0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,11 +38,13 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_24
+        targetCompatibility = JavaVersion.VERSION_24
     }
 
     kotlinOptions {
+        jvmTarget = "24"
+        freeCompilerArgs = listOf()
     }
 
     buildFeatures {
